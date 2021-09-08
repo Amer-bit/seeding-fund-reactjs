52ef1cfc3f91907b8c672216817f63ba86c71685
amer-bit <ny.1995@outlook.com>
2021-09-08 18:08:58 +0300

    created routes for requestin funds and view them

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import Login from "./components/login/login";
 import AdminLogin from "./components/admin-login/admin-login";
 import Register from "./components/registeration/regiester";
+import UserFundRequests from "./components/user-funds/view-fund";
+import FundRequest from "./components/user-funds/create-fund-request";
 import { Switch, Route } from "react-router-dom";
 
 const routes = [
   { component: Login, path: '/login' },
   { component: AdminLogin, path: '/admin/login' },
   { component: Register, path: '/register' },
+  { component: UserFundRequests, path: '/viewmyfunds' },
+  { component: FundRequest, path: '/requestfund' }
 ]
 
 function App() {
