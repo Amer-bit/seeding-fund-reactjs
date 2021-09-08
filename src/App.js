7a55289d099cefb0f6a877a03a76efb3be3186e5
amer-bit <ny.1995@outlook.com>
2021-09-08 10:43:31 +0300

    added admin login route

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import Login from "./components/login/login";
+import AdminLogin from "./components/admin-login/admin-login";
 import Register from "./components/registeration/regiester";
 import { Switch, Route } from "react-router-dom";
 
 const routes = [
   { component: Login, path: '/login' },
-  { component: Register, path: '/register' }
+  { component: AdminLogin, path: '/admin/login' },
+  { component: Register, path: '/register' },
 ]
 
 function App() {
   return (
     <div className="App">
-      <Switch>{routes.map((route, ind) => <Route key={ind} path = {route.path} component = {route.component}/>)}</Switch>
+      <Switch>{routes.map((route, ind) => <Route key={ind} path={route.path} component={route.component} />)}</Switch>
     </div>
   );
 }
