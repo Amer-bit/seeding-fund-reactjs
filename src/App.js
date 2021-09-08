3c71f8149e8a1d31d8879284536797880af2ac4e
amer-bit <ny.1995@outlook.com>
2021-09-08 21:02:36 +0300

    indnting and changing login route to /

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import FundRequest from "./components/user-funds/create-fund-request";
 import { Switch, Route } from "react-router-dom";
 
 const routes = [
-  { component: Login, path: '/login' },
+  { component: Login, path: '/', isExact: true },
   { component: AdminLogin, path: '/admin/login' },
   { component: Register, path: '/register' },
   { component: UserFundRequests, path: '/viewmyfunds' },
@@ -16,7 +16,14 @@ const routes = [
 function App() {
   return (
     <div className="App">
-      <Switch>{routes.map((route, ind) => <Route key={ind} path={route.path} component={route.component} />)}</Switch>
+      <Switch>
+        {routes.map((route, ind) =>
+          <Route key={ind}
+            exact={route.isExact || false}
+            path={route.path}
+            component={route.component} />)
+        }
+      </Switch>
     </div>
   );
 }
