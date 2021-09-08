f2805444f0c37aed6ea7825985785025cd82540d
amer-bit <ny.1995@outlook.com>
2021-09-08 21:07:13 +0300

    added fund reducer

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,9 +1,10 @@
 import {combineReducers, createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import auth from '../reducer/auth';
+import funds from '../reducer/funds';
 
 // I will combine reducers here. 
-let reducers = combineReducers({auth});
+let reducers = combineReducers({auth, funds});
 
 // create my store and pass this reducers variable 
 // and use thunk to be able to call dispatch async
