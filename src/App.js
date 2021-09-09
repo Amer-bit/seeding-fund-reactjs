158c3a7ef651202050232fcf84048d063937d5b5
amer-bit <ny.1995@outlook.com>
2021-09-09 10:11:36 +0300

    added admin routes to view users and the chart

--- src/App.js
+++ src/App.js
@@ -3,14 +3,19 @@ import AdminLogin from "./components/admin-login/admin-login";
 import Register from "./components/registeration/regiester";
 import UserFundRequests from "./components/user-funds/view-fund";
 import FundRequest from "./components/user-funds/create-fund-request";
+import Users from "./components/admin-funds/users";
+import PieChart from "./components/admin-funds/bar-chart";
 import { Switch, Route } from "react-router-dom";
 
 const routes = [
   { component: Login, path: '/', isExact: true },
-  { component: AdminLogin, path: '/admin/login' },
+  { component: AdminLogin, path: '/admin' },
   { component: Register, path: '/register' },
   { component: UserFundRequests, path: '/viewmyfunds' },
-  { component: FundRequest, path: '/requestfund' }
+  { component: FundRequest, path: '/requestfund' },
+  { component: Users, path: '/users' },
+  { component: PieChart, path: '/chart' },
+
 ]
 
 function App() {
