8e2a90961fe982e13d385fa8f5e83d4e308ac960
amer-bit <ny.1995@outlook.com>
2021-09-09 10:36:24 +0300

    imported the reducer from single place

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,10 +1,8 @@
 import {combineReducers, createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import auth from '../reducer/auth';
-import funds from '../reducer/funds';
+import { rootReducer } from '../reducer'
 
-// I will combine reducers here. 
-let reducers = combineReducers({auth, funds});
+let reducers = combineReducers(rootReducer);
 
 // create my store and pass this reducers variable 
 // and use thunk to be able to call dispatch async
